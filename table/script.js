1f978763ca2b14b9b61c696994ebd6cdcf13f14f
laiba shaikh <laibalaiq98@gmail.com>
2025-08-08 19:55:31 +0500

    Add files via upload

---
+++ table/script.js
@@ -0,0 +1,11 @@
+var number = prompt("👋 Hello! Enter the number you want the table for:");
+number = parseInt(number);
+var container = document.getElementById("tableContainer");
+
+ if (!isNaN(number)) {
+    for (var i = 1; i <= 10; i++) {
+      container.innerHTML += `<div class="table-line">🎲 ${number} x ${i} = ${number * i}</div>`;
+    }
+  } else {
+    container.innerHTML = "<div class='table-line'>❌ Please refresh and enter a valid number.</div>";
+  }
