5b332b998e56b932b61963d8b27a2cbd4550a11a
laiba shaikh <laibalaiq98@gmail.com>
2025-08-09 22:35:38 -0700

    Update script.js

--- table/script.js
+++ table/script.js
@@ -1,8 +1,9 @@
-var number = prompt("👋 Hello! Enter the number you want the table for:");
-number = parseInt(number);
-var container = document.getElementById("tableContainer");
+  var number = prompt("👋 Hello! Enter the number you want the table for:");
+  number = parseInt(number);
 
- if (!isNaN(number)) {
+  var container = document.getElementById("tableContainer");
+
+  if (!isNaN(number)) {
     for (var i = 1; i <= 10; i++) {
       container.innerHTML += `<div class="table-line">🎲 ${number} x ${i} = ${number * i}</div>`;
     }
